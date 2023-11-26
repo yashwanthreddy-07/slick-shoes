a4fde41a2e76156df16118c5cfe92ccc40efe6f0
Yashwanth Reddy <yashwanthreddy072@gmail.com>
2023-11-26 14:58:21 +0530

    first commit

---
+++ src/sections/index.jsx
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+import SpecialOffer from "./SpecialOffer";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+
+export {
+    Hero,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    Footer,
+    Subscribe,
+    CustomerReviews,
+    SuperQuality
+}
